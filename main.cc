16bdcb2991d7a17fea4847b12f066484de82f0c1
Artur Mukhamadiev <muhamadiev1@gmail.com>
2025-02-14 17:41:57 +0300

    second

--- main.cc
+++ main.cc
@@ -1,15 +1,20 @@
 #include <iostream>
+#include <wayland-client-protocol.h>
 #include <wayland-client.h>
 
 int main()
 {
+    wl_registry_listener listener;
     wl_display* display = wl_display_connect(nullptr);
 
     if (display) {
         std::cout << "found\n";
     } else {
         std::cout << "not found\n";
+        return 0;
     }
+
+    wl_registry* registry = wl_display_get_registry(display);
 
     return 0;
 }
